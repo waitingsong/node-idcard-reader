2673fdb141b370900f321054aff2cf2125308cd7
waiting <1661926154@qq.com>
2017-08-27 20:51:38 +0800

    Update find device and connect

--- src/bin/idc-reader.ts
+++ src/bin/idc-reader.ts
@@ -1,2 +1,28 @@
 import * as IDCR from '../lib/index';
 
+const settings = {
+    dllTxt: 'd:/sdtapi.dll',
+    dllImage: 'd:/wltrs.dll',
+}
+
+IDCR.init(settings).then((inited) => {
+    if ( ! inited) {
+        return;
+    }
+    const config: IDCR.DeviceConfig = IDCR.find_device();
+
+     if (config.port) {
+         IDCR.connect_device(config);
+         console.log('config:', config);
+     
+         IDCR.find_card(config).then(res => {
+             console.log('found card', res);
+     
+             IDCR.disconnect_device(config.port);
+         }).catch(ex => {
+             console.log(ex);
+             IDCR.disconnect_device(config.port);
+         });
+     }
+});
+
