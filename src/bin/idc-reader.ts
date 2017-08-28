6a7c76b823cbe8c5ae1c035f86c45752a337b191
waiting <1661926154@qq.com>
2017-08-28 11:01:52 +0800

    Rename variable name of config to device

--- src/bin/idc-reader.ts
+++ src/bin/idc-reader.ts
@@ -10,23 +10,23 @@ idcr.init(settings).then((inited) => {
     if ( ! inited) {
         return;
     }
-    const config = idcr.find_device();
+    const device = idcr.find_device();
 
-     if (config.port) {
-         idcr.connect_device(config);
-         console.log('config:', config);
+     if (device.port) {
+         idcr.connect_device(device);
+         console.log('device:', device);
 
-         idcr.find_card(config).then(msg => {
+         idcr.find_card(device).then(msg => {
              console.log('Found card ' + msg);
 
-             const res = idcr.select_card(config);
+             const res = idcr.select_card(device);
 
              console.log('Select card ' + (res ? 'succeed' : 'failed'));
 
-             idcr.disconnect_device(config.port);
+             idcr.disconnect_device(device.port);
          }).catch(ex => {
              console.log(ex);
-             idcr.disconnect_device(config.port);
+             idcr.disconnect_device(device.port);
          });
      }
 });
