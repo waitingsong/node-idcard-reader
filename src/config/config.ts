7ae759529918e13e17d6033690c865f603520392
waiting <1661926154@qq.com>
2017-08-28 10:16:03 +0800

    Update comments

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,18 +4,21 @@ export interface Init {
     findCardRetryTimes?: number;    // 找卡重试数量，间隔1sec
 }
 
+// 初始化参数
 export const init: Init = {
     dllTxt: '',
     dllImage: '',
     findCardRetryTimes: 5,
 };
 
+// dll接口方法
 export interface ApiTxt {
     SDT_OpenPort(port: number): number; // 查找设备并打开端口
     SDT_ClosePort(port: number): number;  // 关闭端口
     SDT_StartFindIDCard(port: number, pucIIN: Buffer, iIfOpen: number): number; // 找卡
 }
 
+// 读卡设置
 export interface Device {
     port: number;   // device connect port
     useUsb: boolean;    // device access mode usb or serial
